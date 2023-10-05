666251fa2a16b9e42555976cb010734e6f1c92dc
FahimSakib <fsakib991@gmail.com>
2023-10-05 21:36:35 +0600

    components

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,24 +1,11 @@
+import Navbar from "../../../components/Navbar";
+
 export default function RestaurantMenuPage() {
     return (
         <main className="bg-gray-100 min-h-screen w-screen text-black">
             <main className="max-w-screen-2xl m-auto bg-white">
-                {/* NAVBAR */}
-                <nav className="bg-white p-2 flex justify-between">
-                    <a href="" className="font-bold text-gray-700 text-2xl">
-                        {" "} OpenTable{" "}
-                    </a>
-                    <div>
-                        <div className="flex">
-                            <button
-                                className="bg-blue-400 text-white border p-1 px-4 rounded mr-3"
-                            >
-                                Sign in
-                            </button>
-                            <button className="border p-1 px-4 rounded">Sign up</button>
-                        </div>
-                    </div>
-                </nav>
-                {/* NAVBAR */} {/* HEADER */}
+                <Navbar />
+                {/* HEADER */}
                 <div className="h-96 overflow-hidden">
                     <div
                         className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center"
@@ -28,7 +15,8 @@ export default function RestaurantMenuPage() {
                         </h1>
                     </div>
                 </div>
-                {/* HEADER */} {/* DESCRIPTION PORTION */}
+                {/* HEADER */} 
+                {/* DESCRIPTION PORTION */}
                 <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
                     <div className="bg-white w-[100%] rounded p-3 shadow">
                         {/* RESAURANT NAVBAR */}
