2f7d49ccbbf49837ab4282529f0dc1f181d16c00
FahimSakib <fsakib991@gmail.com>
2023-10-06 12:16:47 +0600

    pages - refactor

---
+++ app/restaurant/[slug]/components/RestaurantNavBar.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function RestaurantNavBar() {
+    return (
+        <nav className="flex text-reg border-b pb-2">
+            <Link href="/restaurant/abcd" className="mr-7"> Overview </Link>
+            <Link href="/restaurant/abcd/menu" className="mr-7"> Menu </Link>
+        </nav>
+    )
+}
